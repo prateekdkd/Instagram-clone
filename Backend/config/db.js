e8336872edcc303c2955d791dc345362b5e018e5
Prateek <pratk2k7@gmail.com>
2026-06-29 09:22:02 +0530

    Add frontend

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,9 +1,13 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const connectDB = async ()=>{
+   
     try{
+          console.log(process.version);
+    console.log(process.env.MONGO_URI);
          const conn =  await mongoose.connect(process.env.MONGO_URI)
          console.log("DB Connected")
+            console.log("Connected:", conn.connection.host);
     }
     catch (err){
         console.log(err);
